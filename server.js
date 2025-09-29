2f22f1d5141bd83841e5fb2c03e9a14f786a2969
Umarbek Rustamov <rustamovumar0@gmail.com>
2025-09-29 21:05:35 +0500

    d

--- server.js
+++ server.js
@@ -20,6 +20,26 @@ app.get('/home-relocation', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'home-relocation.html'));
 });
 
+app.get('/office-relocation', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'office-relocation.html'));
+});
+
+app.get('/furniture-installation', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'furniture-installation.html'));
+});
+
+app.get('/cleaning', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'cleaning.html'));
+});
+
+app.get('/packing', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'packing.html'));
+});
+
+app.get('/special-movings', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'special-movings.html'));
+});
+
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
 });
